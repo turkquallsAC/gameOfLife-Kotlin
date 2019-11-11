a239411f6674bb4d8ca9ec53229b2ecabe37146f
Turk <turk.qualls@accenture.com>
2019-11-11 12:14:28 -0600

    get potential neighbors

--- src/test/kotlin/boardTest.kt
+++ src/test/kotlin/boardTest.kt
@@ -39,4 +39,21 @@ class boardTest {
 
         assertThat(board.getNeighbors(cell1), empty())
     }
+
+    @Test
+    fun cellCanOnlyHaveUpToEightNeighbors() {
+        val board = Board()
+        addNeighborhood(board)
+
+        assertThat(board.getCells(), hasSize(25))
+        assertThat(board.getNeighbors(Cell(0,0)), hasSize(8))
+    }
+
+    private fun addNeighborhood(board: Board) {
+        (-2..2).forEach { x ->
+            (-2..2).forEach {y ->
+                board.addCells(Cell(x, y))
+            }
+        }
+    }
 }
